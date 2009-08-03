963a17ff919b94e82d13f786d69391878cf383da
tsaikd <tsaikd@gmail.com>
2009-08-03 15:07:40 +0800

    update lang

--- lang/KDWall_zh_TW.ts
+++ lang/KDWall_zh_TW.ts
@@ -2,22 +2,104 @@
 <!DOCTYPE TS><TS version="1.1" language="zh_TW">
 <defaultcodec></defaultcodec>
 <context>
+    <name></name>
+    <message>
+        <location filename="../src/PicFinder.cpp" line="12"/>
+        <source>System does not support image format %1</source>
+        <translation>系統不支援 %1 影像格式</translation>
+    </message>
+</context>
+<context>
+    <name>QConfMainApp</name>
+    <message>
+        <location filename="../src/ConfMainApp.cpp" line="128"/>
+        <source>Window Manager auto detect failed</source>
+        <translation>視窗管理員自動偵測失敗</translation>
+    </message>
+</context>
+<context>
     <name>QDBMgr</name>
     <message>
-        <location filename="../src/DBMgr.cpp" line="24"/>
+        <location filename="../src/DBMgr.cpp" line="25"/>
         <source>SQLITE not supported</source>
         <translation>系統不支援 SQlite 資料庫</translation>
     </message>
     <message>
-        <location filename="../src/DBMgr.cpp" line="35"/>
+        <location filename="../src/DBMgr.cpp" line="36"/>
         <source>Open database failed</source>
         <translation>開啟資料庫失敗</translation>
     </message>
 </context>
 <context>
+    <name>QWallCacheMaker</name>
+    <message>
+        <location filename="../src/WallMgr.cpp" line="89"/>
+        <source>Create tmp file failed</source>
+        <translation>建立暫存檔失敗</translation>
+    </message>
+    <message>
+        <location filename="../src/WallMgr.cpp" line="95"/>
+        <source>Load image failed (&apos;%1&apos;)</source>
+        <translation>讀取影像檔失敗 (&apos;%1&apos;)</translation>
+    </message>
+    <message>
+        <location filename="../src/WallMgr.cpp" line="105"/>
+        <source>Save image to BMP failed (&apos;%1&apos;)</source>
+        <translation>儲存 BMP 影像檔失敗 (&apos;%1&apos;)</translation>
+    </message>
+</context>
+<context>
+    <name>QWinConfMainApp</name>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="14"/>
+        <source>Start without window</source>
+        <translation>啟動時隱藏視窗</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="16"/>
+        <source>Picture history max number</source>
+        <translation>影像歷史最大數量</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="18"/>
+        <source>Wallpaper change timer (second)</source>
+        <translation>更換背景時間間隔 (秒)</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="19"/>
+        <source>Cache image max number</source>
+        <translation>快取影像最大數量</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="20"/>
+        <source>Disable cache warning</source>
+        <translation>取消快取警告訊息</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="22"/>
+        <source>Ignore image format support warning</source>
+        <translation>忽略支援影像格式的警告</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="23"/>
+        <source>Picture finder refresh timer (msec)</source>
+        <translation>影像搜尋器的更新時間 (毫秒)</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="29"/>
+        <source>&amp;Ok</source>
+        <translation>確定(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="35"/>
+        <source>&amp;Cancel</source>
+        <translation>取消(&amp;C)</translation>
+    </message>
+</context>
+<context>
     <name>QWinMainApp</name>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="33"/>
+        <location filename="../src/WinMainApp.cpp" line="34"/>
         <source>&amp;Random Picture</source>
         <translation>隨機圖片(&amp;R)</translation>
     </message>
@@ -27,74 +109,79 @@
         <translation type="obsolete">離開(&amp;X)</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="43"/>
+        <location filename="../src/WinMainApp.cpp" line="44"/>
         <source>Profile:</source>
         <translation>設定檔:</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="60"/>
+        <location filename="../src/WinMainApp.cpp" line="65"/>
         <source>Path</source>
         <translation>路徑</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="61"/>
+        <location filename="../src/WinMainApp.cpp" line="66"/>
         <source>Count</source>
         <translation>數量</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="70"/>
+        <location filename="../src/WinMainApp.cpp" line="77"/>
         <source>+</source>
         <translation>+</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="72"/>
+        <location filename="../src/WinMainApp.cpp" line="79"/>
         <source>Add other picture dir</source>
         <translation>增加其他圖片資料夾</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="78"/>
+        <location filename="../src/WinMainApp.cpp" line="85"/>
         <source>-</source>
         <translation>-</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="80"/>
+        <location filename="../src/WinMainApp.cpp" line="87"/>
         <source>Remove selected picture dir</source>
         <translation>移除選擇的圖片資料夾</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="86"/>
+        <location filename="../src/WinMainApp.cpp" line="93"/>
         <source>R</source>
         <translation>R</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="88"/>
+        <location filename="../src/WinMainApp.cpp" line="95"/>
         <source>Search selected picture dir again</source>
         <translation>重新搜尋選擇的圖片資料夾</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="100"/>
+        <location filename="../src/WinMainApp.cpp" line="107"/>
         <source>&amp;RandPic</source>
         <translation>隨機圖片(&amp;R)</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="101"/>
+        <location filename="../src/WinMainApp.cpp" line="108"/>
         <source>Select a random picture from database and setup wallpaper</source>
         <translation>從資料庫中隨機選擇圖片, 並設定為桌布</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="107"/>
+        <location filename="../src/WinMainApp.cpp" line="120"/>
         <source>&amp;Quit</source>
         <translation>離開(&amp;Q)</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="228"/>
+        <location filename="../src/WinMainApp.cpp" line="242"/>
         <source>Add picture directory</source>
         <translation>增加圖片資料夾</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="303"/>
+        <location filename="../src/WinMainApp.cpp" line="331"/>
         <source>Changing: %1</source>
         <translation>設定桌布中: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/WinMainApp.cpp" line="114"/>
+        <source>&amp;Config</source>
+        <translation>設定(&amp;C)</translation>
     </message>
 </context>
 </TS>
